0ec75c28868c3aa4c80c72319b27b59713844d1f
Quintero <m.alejandro.quinter@gmail.com>
2022-02-26 17:57:55 -0600

    first endpoints

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ app.get('/',(req, res) => {
     res.send('Hola first endpoint')
 });
 
+app.get('/users',(req, res) => {
+    res.json([
+        {
+            user: 'Quintero',
+            id: '1',
+            password: 'Test123',
+        },
+        {
+            user: 'Alberto',
+            id: '2',
+            password: 'pass123',
+        },
+        {
+            user: 'Alex',
+            id: '3',
+            password: 'alex321',
+        }
+    ]);
+});
+
+app.get('/users/:id')
+
 app.listen(port, () => {
     console.log('el puerto es:' + port);
 });
